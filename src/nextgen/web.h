50ade4efc1285c8307e1df746eb9aa4a47b16c16
Eric Muyser <eric.muyser@gmail.com>
2010-01-21 03:02:59 -0800

    Add web classes.

---
+++ src/nextgen/web.h
@@ -0,0 +1,35 @@
+#ifndef NEXTGEN_WEB_H
+#define NEXTGEN_WEB_H
+
+#include "nextgen/common.h"
+#include "nextgen/network.h"
+
+namespace nextgen
+{
+    namespace web
+    {
+        class url
+        {
+
+        };
+
+        class agent_provider
+        {
+
+        };
+
+        class proxy_provider
+        {
+
+
+        };
+
+        class crawler
+        {
+
+        };
+    }
+
+}
+
+#endif
